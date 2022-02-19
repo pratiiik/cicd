7c4d1ad2d6ebbad7f67fdb28c510f05e9c4ed3eb
pratiiik <39582672+pratiiik@users.noreply.github.com>
2022-02-19 23:10:21 +0530

    Create nodejs.groovy

---
+++ nodejs.groovy
@@ -0,0 +1,21 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/pratiiik/myonly.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('pratiiik')
+            node / gitConfigEmail('pratiiikyadav@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('Nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
